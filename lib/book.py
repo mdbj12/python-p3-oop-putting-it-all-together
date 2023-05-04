e983e89bacb9a450c2411cba713a85b7ab79aad4
Michael Jeung <michaelj229@gmail.com>
2023-05-03 22:37:47 -0400

    done

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count=0):
+        self.title = title
+        self.page_count = page_count
+
+    def turn_page(self):
+        print('Flipping the page...wow, you read fast!')
+    
+    def get_count(self):
+        return self._page_count # create a private parameter
+    
+    def set_count(self, value):
+        if type(value) == int:
+            self._page_count = value # create a private parameter
+        
+        print("page_count must be an integer")
+    page_count = property(get_count, set_count) # property()
